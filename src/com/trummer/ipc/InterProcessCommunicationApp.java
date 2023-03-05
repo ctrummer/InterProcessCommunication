e049682eb87757ab689804025386443b18ea9234
christiantrummer <christian.trummer@chello.at>
2023-03-05 18:24:18 +0100

    First Changes

---
+++ src/com/trummer/ipc/InterProcessCommunicationApp.java
@@ -0,0 +1,7 @@
+package com.trummer.ipc;
+
+public class InterProcessCommunicationApp {
+    public static void main(String[] args) {
+        System.out.println("Inter Process Communication");
+    }
+}
